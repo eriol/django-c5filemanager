04ecd40fdade353e34081fe22c69641c05a97a64
Daniele Tricoli <eriol@mornie.org>
2010-09-27 09:12:29 +0200

    Changed default upload directory

--- c5filemanager/settings.py
+++ c5filemanager/settings.py
@@ -4,4 +4,4 @@ from django.conf import settings
 MEDIA_ROOT = getattr(settings, 'C5FILEMANAGER_MEDIA_ROOT', settings.MEDIA_ROOT)
 
 # c5filemanager path relative to media root
-C5FILEMANAGER_DIR = getattr(settings, 'C5FILEMANAGER_DIR', 'c5filemanager')
+C5FILEMANAGER_DIR = getattr(settings, 'C5FILEMANAGER_DIR', 'upload')
