f22047de39a1f4ee15552b76af3607edf87302c0
Daniele Tricoli <eriol@mornie.org>
2010-10-11 19:51:58 +0200

    Added an app level MEDIA_URL

--- c5filemanager/settings.py
+++ c5filemanager/settings.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 
 MEDIA_ROOT = getattr(settings, 'C5FILEMANAGER_MEDIA_ROOT', settings.MEDIA_ROOT)
+MEDIA_URL = getattr(settings, 'C5FILEMANAGER_MEDIA_URL', settings.MEDIA_URL)
 
 # c5filemanager path relative to media root
 C5FILEMANAGER_DIR = getattr(settings, 'C5FILEMANAGER_DIR', 'upload')
