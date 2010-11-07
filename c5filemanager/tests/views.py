1887efd31defbf339956e6af35370c2fdf551085
Daniele Tricoli <eriol@mornie.org>
2010-11-07 08:29:47 +0100

    Refactored rename and getfolder view tests

--- c5filemanager/tests/views.py
+++ c5filemanager/tests/views.py
@@ -43,6 +43,7 @@ def create_file_info_for_mock(exists_mock,
 
     return info
 
+
 class CreateFileInfoTest(TestCase):
 
     def test_create_file_info_for_txt(self):
@@ -113,6 +114,7 @@ class CreateFileInfoTest(TestCase):
 
         self.failUnlessEqual(info, expected_result)
 
+
 class FilemanagerTestCase(TestCase):
     urls = 'c5filemanager.tests.urls'
 
@@ -120,30 +122,44 @@ class FilemanagerTestCase(TestCase):
         self.mockify()
 
 
-class ViewsTest(FilemanagerTestCase):
+class GetFolderTest(FilemanagerTestCase):
+    """Tests for c5filemanager.views.getfolder"""
 
-    def mockify(self, mock_hook=Mock()):
+    def mockify(self):
         pass
 
-    def test_getfolder(self):
-        """Test getfolder."""
+    def test_getfolder_success(self):
+        """Test succesful retrieving of a directory."""
         # ?path=/&mode=getfolder&showThumbs=true
         response = self.client.get('', {'path': '/',
                                         'mode': 'getfolder',
                                         'showThumbs': 'true'})
         self.failUnlessEqual(response.status_code, 200)
 
-    @patch('shutil.move')
-    @patch('os.path.exists')
-    def test_rename(self, exists_mock, shmove_mock):
-        """Test rename of a file or directory."""
-        exists_mock.return_value = True
-        #?mode=rename&old=/oldfile.txt&new=newfile.txt
-        response = self.client.get('', {'mode': 'rename',
-                                        'old': 'oldfile.txt',
-                                        'new': 'newfile.txt'})
 
-        self.failUnlessEqual(response.status_code, 200)
+class RenameTest(FilemanagerTestCase):
+    """Tests for c5filemanager.views.rename"""
+
+    def setUp(self):
+        self.exists_mock = Mock()
+        self.exists_mock.return_value = True
+        self.move_mock = Mock()
+        self.mockify(exists_mock=self.exists_mock, move_mock=self.move_mock)
+
+    def mockify(self, exists_mock=Mock(), move_mock=Mock()):
+        @patch('shutil.move', move_mock)
+        @patch('os.path.exists', exists_mock)
+        def patching():
+            #?mode=addfolder&path=/&name=new_directory
+            self.response = self.client.get('', {'mode': 'rename',
+                                                 'old': 'oldfile.txt',
+                                                 'new': 'newfile.txt'})
+        patching()
+
+    def test_rename_success(self):
+        """Test succesful rename of a file (or directory)."""
+        self.failUnlessEqual(self.response.status_code, 200)
+        self.failUnless(self.move_mock.called)
 
         expected_content = {
             'Code': 0,
@@ -157,23 +173,23 @@ class ViewsTest(FilemanagerTestCase):
             'Error': 'No Error',
             'Old Name': 'oldfile.txt'}
 
-        self.failUnlessEqual(response.content,
+        self.failUnlessEqual(self.response.content,
                              simplejson.dumps(expected_content))
 
-        # If the file does't exists an error message is returned.
-        exists_mock.return_value = False
-        response = self.client.get('', {'mode': 'rename',
-                                        'old': 'notexists.txt',
-                                        'new': 'newfile.txt'})
+    def test_rename_fail(self):
+        """Test unsuccesful rename of a file (or directory)."""
+        self.failUnlessEqual(self.response.status_code, 200)
+
+        self.exists_mock.return_value = False
+        self.mockify(exists_mock=self.exists_mock)
 
         expected_content = {'Code': -1, 'Error': 'No such file or directory'}
-
-        self.failUnlessEqual(response.content,
+        self.failUnlessEqual(self.response.content,
                              simplejson.dumps(expected_content))
 
 
 class DeleteTest(FilemanagerTestCase):
-    """Tests for c5filemanager.views.delete."""
+    """Tests for c5filemanager.views.delete"""
 
     def setUp(self):
         self.exists_mock = Mock()
@@ -188,6 +204,7 @@ class DeleteTest(FilemanagerTestCase):
         @patch('os.path.isdir', isdir_mock)
         @patch('os.path.exists', exists_mock)
         def patching():
+            #?mode=delete&path=/file-to-be-deleted.txt
             query = {'mode': 'delete', 'path': path}
             self.response = self.client.get('', query)
         patching()
@@ -201,6 +218,7 @@ class DeleteTest(FilemanagerTestCase):
                      remove_mock=self.remove_mock,
                      path='/file-to-be-deleted.txt')
 
+        self.failUnlessEqual(self.response.status_code, 200)
         self.failUnless(self.exists_mock.called)
         self.failUnless(self.remove_mock.called)
 
@@ -219,6 +237,7 @@ class DeleteTest(FilemanagerTestCase):
                      rmdir_mock=self.rmdir_mock,
                      path='/directory-to-be-deleted/')
 
+        self.failUnlessEqual(self.response.status_code, 200)
         self.failUnless(self.exists_mock.called)
         self.failUnless(self.rmdir_mock.called)
 
@@ -234,13 +253,15 @@ class DeleteTest(FilemanagerTestCase):
         self.mockify(exists_mock=self.exists_mock,
                      path='/nofile-or-directory-to-be-deleted')
 
+        self.failUnlessEqual(self.response.status_code, 200)
+
         expected_content = {'Code': -1, 'Error': 'No such file or directory'}
         self.failUnlessEqual(self.response.content,
                              simplejson.dumps(expected_content))
 
 
 class UploadFileTest(FilemanagerTestCase):
-    """Tests for c5filemanager.views.add."""
+    """Tests for c5filemanager.views.add"""
 
     def mockify(self, mock_hook=Mock()):
         @patch('c5filemanager.views.handle_uploaded_file', mock_hook)
@@ -257,6 +278,7 @@ class UploadFileTest(FilemanagerTestCase):
 
     def test_add_success(self):
         """Test succesful upload of a file."""
+        self.failUnlessEqual(self.response.status_code, 200)
         self.failUnless(self.handle_uploaded_file_mock.called)
 
         expected_content = {'Code': 0,
@@ -269,6 +291,8 @@ class UploadFileTest(FilemanagerTestCase):
 
     def test_add_fail(self):
         """Test unsuccesful upload of a file."""
+        self.failUnlessEqual(self.response.status_code, 200)
+
         handle_uploaded_file_mock = Mock()
         handle_uploaded_file_mock.side_effect = IOError(2, 'No such file!')
         self.mockify(handle_uploaded_file_mock)
@@ -280,7 +304,7 @@ class UploadFileTest(FilemanagerTestCase):
 
 
 class AddFolderTest(FilemanagerTestCase):
-    """Tests for c5filemanager.views.addfolder."""
+    """Tests for c5filemanager.views.addfolder"""
 
     def mockify(self, mock_hook=Mock()):
         @patch('os.mkdir', mock_hook)
@@ -294,6 +318,7 @@ class AddFolderTest(FilemanagerTestCase):
 
     def test_addfolder_success(self):
         """Test succesful creation of a new directory."""
+        self.failUnlessEqual(self.response.status_code, 200)
         self.failUnless(self.mkdir_mock.called)
 
         expected_content = {'Code': 0,
@@ -306,6 +331,8 @@ class AddFolderTest(FilemanagerTestCase):
     def test_addfolder_fail(self):
         """Test unsuccessful creation of a new directory."""
         # Raise an OSError exception during directory creation.
+        self.failUnlessEqual(self.response.status_code, 200)
+
         mkdir_mock = Mock()
         mkdir_mock.side_effect = OSError(2, 'File exists')
         self.mockify(mkdir_mock)
