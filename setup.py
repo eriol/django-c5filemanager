c287f86bfeab56c3ad932833c1da487df5909f22
Daniele Tricoli <eriol@mornie.org>
2010-11-16 17:17:42 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+from c5filemanager import VERSION
+
+setup(
+    name='django-c5filemanager',
+    version='.'.join(str(v) for v in VERSION),
+    description='Django connector for Code Five Filemanager.',
+    author='Daniele Tricoli',
+    author_email='eriol@mornie.org',
+    packages=find_packages(),
+    package_data = {
+        'c5filemanager': [
+            'locale/*/LC_MESSAGES/*',
+        ],
+    },
+)
