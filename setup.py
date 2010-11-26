71eebaf13f636e980d3c9bc64a0c9ca82ea1138a
Daniele Tricoli <eriol@mornie.org>
2010-11-26 04:28:14 +0100

    Fixed description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from c5filemanager import VERSION
 setup(
     name='django-c5filemanager',
     version='.'.join(str(v) for v in VERSION),
-    description='Django connector for Code Five Filemanager.',
+    description='Django connector for Core Five Filemanager.',
     author='Daniele Tricoli',
     author_email='eriol@mornie.org',
     packages=find_packages(),
