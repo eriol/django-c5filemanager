f635ae8be3dacaecda7aaa7498f7e148767b959d
Daniele Tricoli <eriol@mornie.org>
2010-12-01 11:23:52 +0100

    Enhanced setup.py

--- c5filemanager/__init__.py
+++ c5filemanager/__init__.py
@@ -7,3 +7,7 @@ Copyright (c) 2010 Daniele Tricoli <eriol@mornie.org>
 Read LICENSE for more informations.
 """
 VERSION = (0, 1, 0)
+
+def get_version():
+    """Returns project version in a human readable form."""
+    return '.'.join(str(v) for v in VERSION)
--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
+"""django-c5filemanager setup file.
+
+THIS SOFTWARE IS UNDER BSD LICENSE.
+Copyright (c) 2010 Daniele Tricoli <eriol@mornie.org>
+
+Read LICENSE for more informations.
+"""
 from setuptools import setup, find_packages
 
-from c5filemanager import VERSION
+from c5filemanager import get_version
+
+download_page = 'http://downloads.mornie.org/django-c5filemanager/'
 
 setup(
     name='django-c5filemanager',
-    version='.'.join(str(v) for v in VERSION),
+    version=get_version(),
     description='Django connector for Core Five Filemanager.',
+    long_description=open('README').read(),
     author='Daniele Tricoli',
     author_email='eriol@mornie.org',
+    url='http://mornie.org/projects/django-c5filemanager/'
+    download_url='%sdjango-c5filemanager-%s.tar.gz' % (download_page,
+                                                       get_version())
     packages=find_packages(),
     package_data = {
         'c5filemanager': [
@@ -17,5 +30,15 @@ setup(
     },
     install_requires = [
         'simplejson>=2.1.0',
+        'PIL',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
     ],
 )
