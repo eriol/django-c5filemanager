cde8d10442d959a9844aeaf90a2aa245277ace82
Daniele Tricoli <eriol@mornie.org>
2012-12-12 10:31:13 +0100

    Updated comment

--- c5filemanager/settings.py
+++ c5filemanager/settings.py
@@ -14,7 +14,7 @@ from django.conf import settings
 MEDIA_ROOT = getattr(settings, 'C5FILEMANAGER_MEDIA_ROOT', settings.MEDIA_ROOT)
 MEDIA_URL = getattr(settings, 'C5FILEMANAGER_MEDIA_URL', settings.MEDIA_URL)
 
-# django-c5filemanager media path. Must be relative to MEDIA_ROOT.
+# django-c5filemanager media path. Must be relative to STATIC_ROOT.
 C5FILEMANAGER_MEDIA = getattr(settings, 'C5FILEMANAGER_MEDIA', 'filemanager')
 
 # django-c5filemanager upload directory. Must be relative to MEDIA_ROOT.
