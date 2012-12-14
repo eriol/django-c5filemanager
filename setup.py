4c1884cab69d1bdc13590d8aca98d53b1f563a8b
Daniele Tricoli <eriol@mornie.org>
2012-12-14 08:49:14 +0100

    More classifiers

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ Intended Audience :: Developers
 License :: OSI Approved :: BSD License
 Operating System :: OS Independent
 Programming Language :: Python
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
 Framework :: Django
 '''.strip().splitlines()
 
